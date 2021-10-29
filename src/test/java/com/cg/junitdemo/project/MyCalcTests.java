f1d3f46899fb46c7779ba18203103b7a485bc133
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-29 16:39:01 +0530

    C

--- src/test/java/com/cg/junitdemo/project/MyCalcTests.java
+++ src/test/java/com/cg/junitdemo/project/MyCalcTests.java
@@ -1,6 +1,7 @@
 package com.cg.junitdemo.project;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
@@ -11,13 +12,26 @@ public class MyCalcTests {
 	@Test
 	public void testGetPi() {
 		System.out.println("testGetPi");
-
 		MyCalc calc = new MyCalc();
-
 		double expected = 3.142;
 		double actual = calc.getPi();
-
 		assertEquals(expected, actual, 0);
+	}
+
+	@Test
+	public void testGetPi2() {
+		System.out.println("testGetPi2");
+		MyCalc calc = new MyCalc();
+		double unexpected = 1.234;
+		double actual = calc.getPi();
+		assertNotEquals(unexpected, actual, 0);
+	}
+
+	@Test
+	public void testAddNums() {
+		System.out.println("testAddNums");
+		MyCalc calc = new MyCalc();
+		assertEquals(30, calc.addNums(10, 20));
 	}
 
 }
