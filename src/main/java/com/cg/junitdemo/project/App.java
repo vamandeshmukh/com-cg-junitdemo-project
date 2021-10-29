82f31a585e1af99dd0737770e956dc5e0fe2fc27
vamandeshmukh <vamandeshmukh@gmail.com>
2021-10-29 15:52:09 +0530

    l

--- src/main/java/com/cg/junitdemo/project/App.java
+++ src/main/java/com/cg/junitdemo/project/App.java
@@ -3,11 +3,14 @@ package com.cg.junitdemo.project;
 public class App {
 
 	public static void main(String[] args) {
+
 		System.out.println("Start");
 
 		MyCalc myCalc = new MyCalc();
 		System.out.println(myCalc.getPi());
 		System.out.println(myCalc.addNums(10, 20));
+
+		System.out.println("Start");
 	}
 
 }
