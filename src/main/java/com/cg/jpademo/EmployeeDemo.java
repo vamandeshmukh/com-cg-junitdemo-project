02ca536b952b9ddafa650efbe75eed486272f8ec
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-09 10:55:44 +0530

    C
    C

--- src/main/java/com/cg/jpademo/EmployeeDemo.java
+++ src/main/java/com/cg/jpademo/EmployeeDemo.java
@@ -4,6 +4,12 @@ public class EmployeeDemo {
 
 	public static void main(String[] args) {
 
+		System.out.println("Start");
+
+		Employee emp1 = new Employee(101, "Sonu", 10.6);
+
+		System.out.println("End");
+
 	}
 
 }
