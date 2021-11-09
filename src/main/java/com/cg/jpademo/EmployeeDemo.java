109a9bc930e189bfb6b28e170c7472871c575509
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-09 10:26:07 +0530

    C

--- src/main/java/com/cg/jpademo/EmployeeDemo.java
+++ src/main/java/com/cg/jpademo/EmployeeDemo.java
@@ -1,25 +1,9 @@
 package com.cg.jpademo;
 
-import javax.persistence.EntityManager;
-import javax.persistence.EntityManagerFactory;
-import javax.persistence.Persistence;
-
 public class EmployeeDemo {
 
-	private static EntityManagerFactory factory;
-	private static EntityManager entityManager;
+	public static void main(String[] args) {
 
-	static {
-		factory = Persistence.createEntityManagerFactory("abcd");
-		System.out.println("persistance.xml accessed.");
-	}
-
-	public static EntityManager getEntityManager() {
-		if (entityManager == null || !entityManager.isOpen()) {
-			entityManager = factory.createEntityManager();
-		}
-		System.out.println("entityManager object created.");
-		return entityManager;
 	}
 
 }
