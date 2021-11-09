00938b903064e8a5de517caac22ba6c9b2641cae
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-09 14:21:11 +0530

    Commit
    Commit

--- src/main/java/com/cg/jpademo/Employee.java
+++ src/main/java/com/cg/jpademo/Employee.java
@@ -2,8 +2,10 @@ package com.cg.jpademo;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
+//@Table(name = "emp99")
 public class Employee {
 
 	@Id
--- src/main/java/com/cg/jpademo/EmployeeDemo.java
+++ src/main/java/com/cg/jpademo/EmployeeDemo.java
@@ -16,13 +16,22 @@ public class EmployeeDemo {
 
 		tr.begin();
 
-		Employee emp1 = new Employee(101, "Sonu", 10.6);
-		em.persist(emp1); // insert
+//		Employee emp1 = new Employee(101, "Sonu", 10.6);
+		Employee emp2 = new Employee(102, "Monu", 18.5);
+//		Employee emp3 = new Employee(103, "Tonu", 15.2);
+//		em.persist(emp1); // insert
+//		em.persist(emp2);
+//		em.persist(emp3);
+//		em.detach(emp2);
 
-// 		em.remove(emp1); // delete
-//		em.merge(emp1); // update
-		Employee emp2 = em.find(Employee.class, 505); // select
+		emp2.setFirstName("Aaa");
+		em.merge(emp2); // update
 		System.out.println(emp2.toString());
+
+		em.remove(emp2); // delete
+
+		Employee emp4 = em.find(Employee.class, 101); // select
+		System.out.println(emp4.toString());
 		tr.commit();
 
 		System.out.println("End");
