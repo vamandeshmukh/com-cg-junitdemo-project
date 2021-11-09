4b0878064bc45bc73469eb80acfd00c7aeed7ece
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-09 11:12:55 +0530

    C

--- src/main/java/com/cg/jpademo/EmployeeDemo.java
+++ src/main/java/com/cg/jpademo/EmployeeDemo.java
@@ -1,12 +1,20 @@
 package com.cg.jpademo;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class EmployeeDemo {
 
 	public static void main(String[] args) {
-
 		System.out.println("Start");
 
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("abcd");
+		EntityManager em = emf.createEntityManager();
+
 		Employee emp1 = new Employee(101, "Sonu", 10.6);
+
+		em.persist(emp1);
 
 		System.out.println("End");
 
