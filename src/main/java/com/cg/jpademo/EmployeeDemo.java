042ad1ab4d07a97ab75ad56e397e5b71b6e2b5b7
vamandeshmukh <vamandeshmukh@gmail.com>
2021-11-09 12:32:07 +0530

    C

--- src/main/java/com/cg/jpademo/EmployeeDemo.java
+++ src/main/java/com/cg/jpademo/EmployeeDemo.java
@@ -2,6 +2,7 @@ package com.cg.jpademo;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
 
 public class EmployeeDemo {
@@ -9,12 +10,20 @@ public class EmployeeDemo {
 	public static void main(String[] args) {
 		System.out.println("Start");
 
-		EntityManagerFactory emf = Persistence.createEntityManagerFactory("abcd");
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("sridhar");
 		EntityManager em = emf.createEntityManager();
+		EntityTransaction tr = em.getTransaction();
+
+		tr.begin();
 
 		Employee emp1 = new Employee(101, "Sonu", 10.6);
 
-		em.persist(emp1);
+		em.persist(emp1); // insert
+
+//		em.remove(emp1); // delete
+//		em.merge(emp1); // update
+//		em.find(Employee.class, 101); // select
+		tr.commit();
 
 		System.out.println("End");
 
